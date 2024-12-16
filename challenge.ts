932807c6d3aad25ab7db46e086de11795c12ac66
Agattepaille <amelie.gattepaille@hotmail.fr>
2024-12-16 15:34:07 +0100

    ok

--- challenge.ts
+++ challenge.ts
@@ -6,15 +6,15 @@ interface User {
 }
 
 const prettyPrintWilder = (users: User[]): void => {
-    users.map((user: User) => {
+    users.map((user) => {
       console.log(`${user.name} is ${user.age} years old`);
     });
   };
   
   const wilders: User[] = [];
-  const user1 = { name: "Pierre", age: 23 };
-  const user2 = { name: "Paul", age: 34 };
-  const user3 = { name: "Jacques", age: 25 };
+  const user1: User = { name: "Pierre", age: 23 };
+  const user2: User = { name: "Paul", age: 34 };
+  const user3: User = { name: "Jacques", age: 25 };
   wilders.push(user1);
   wilders.push(user2);
   wilders.push(user3);
